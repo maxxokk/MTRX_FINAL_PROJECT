f3a507a3387820298f834a84d7350ceddbe144f8
MarcoJara <162090860+MarcoJara@users.noreply.github.com>
2024-05-21 20:09:43 +1000

    Update flex.h

--- FLEX_RESISTOR_SYS/Inc/flex.h
+++ FLEX_RESISTOR_SYS/Inc/flex.h
@@ -3,11 +3,14 @@
 
 #include <stdint.h>
 
+// Function declarations for enabling clocks, initializing the board, reading ADC values, 
+// continuous ADC reading, GPIO setup for PA3, and handling ADC value changes
+
 void enable_clocks();
 
 void initialise_board();
 
-uint16_t ReadADC(uint32_t channel) ;
+uint16_t ReadADC(uint32_t channel);
 
 void ContinuousReadSingleChannelADC();
 
@@ -15,15 +18,12 @@ void setup_gpio_pa3();
 
 void on_adc_value_change();
 
+// Function declarations for configuring and starting PWM for buzzers on PA8 and PA9
 
-
-
-////buzzers/////
 void setup_timer2_pwm(void);
 
 void start_pwm_2_buzzers(uint32_t frequency, uint32_t duty_cycle);
 
 void setup_gpio_PA8_PA9(void);
-
 
 #endif
